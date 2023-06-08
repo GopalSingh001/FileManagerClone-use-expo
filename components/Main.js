28539e39463437ca3720e540aae382a5ae09b874
Gopal <gt865037@gmail.com>
2023-06-08 18:26:31 +0530

    working on  If someone creates duplicate file or folder then it should not be saved in file-manager

--- components/Main.js
+++ components/Main.js
@@ -1,3 +1,5 @@
+
+
 import React, { useEffect, useState } from 'react';
 import {
     View,
@@ -8,10 +10,15 @@ import {
     FlatList,
     Image,
     ScrollView,
+    ImageBackground,
 } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as FileSystem from 'expo-file-system';
-    
+
+  
+
+
+
 const Main = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [modalVisible2, setModalVisible2] = useState(false);
@@ -23,6 +30,9 @@ const Main = () => {
     const requestStoragePermission = async () => {
         try {
             const { status } = await Permissions.askAsync(Permissions.READ_EXTERNAL_STORAGE);
+            // const permissions = await FileSystem.requestPermissionsAsync();
+            // const permissions = await SAF.requestPermissionsAsync();
+
             if (status === 'granted') {
                 console.log('You can use the storage');
                 getAllFolders(currentPath);
@@ -38,7 +48,20 @@ const Main = () => {
         requestStoragePermission();
     }, []);
 
-  
+    const getAllFolders = async (path) => {
+        try {
+            const result = await FileSystem.readDirectoryAsync(path);
+            console.log('GOT RESULT', result);
+            setFolders(result.map((name) => ({ name, path: `${path}/${name}` })));
+        } catch (err) {
+            console.log(err.message, err.code);
+        }
+    };
+
+    const isFolder = (name) => {
+        const itsfolder = name.includes('.')
+        return itsfolder;
+    };
 
     const createFolder = async () => {
 
@@ -65,11 +88,13 @@ const Main = () => {
     const deleteDir = async (path) => {
         try {
             await FileSystem.deleteAsync(path);
-            
+            getAllFolders(currentPath);
         } catch (error) {
             console.log(error);
         }
     };
+
+ 
 
     return (
         <View style={{ flex: 1 }}>
@@ -79,12 +104,13 @@ const Main = () => {
                         style={{ fontWeight: '700' }}
                         onPress={() => {
                             setCurrentPath(FileSystem.documentDirectory);
-                            
+                            getAllFolders(FileSystem.documentDirectory);
                         }}
                     >
                         Back
                     </Text>
                 )}
+                 
                 <Text style={{ marginLeft: 20 }}>{currentPath}</Text>
             </View>
             <View style={{ marginTop: 50 }}>
@@ -96,17 +122,32 @@ const Main = () => {
                             <TouchableOpacity
                                 style={{
                                     width: '50%',
+                                    
                                     justifyContent: 'center',
                                     alignItems: 'center',
                                     height: 100,
                                 }}
-                                
+                                onPress={() => {
+                                    if (!isFolder(item.name)) {
+                                        setCurrentPath(item.path);
+                                        getAllFolders(item.path);
+                                    }
+                                }}
                                 onLongPress={() => {
                                     deleteDir(item.path);
                                 }}
                             >
-                               
-                                
+                                {isFolder(item.name) ? (
+                                    <Image
+                                        source={require("../images/file.png")}
+                                        style={{ width: 50, height: 50 }}
+                                    />
+                                ) : (
+                                    <Image
+                                        source={require("../images/open-folder.png")}
+                                        style={{ width: 50, height: 50 }}
+                                    />
+                                )}
                                 <Text>
                                     {item.name.length > 20 ? `${item.name.substring(0, 10)}...` : item.name}
                                 </Text>
@@ -116,15 +157,16 @@ const Main = () => {
                     />
                 </ScrollView>
             </View>
+            
             <TouchableOpacity
                 style={{
                     position: 'absolute',
                     right: 20,
                     bottom: 50,
-                    backgroundColor: '#000',
+                    // backgroundColor: '#000',
                     width: 50,
                     height: 50,
-                    borderRadius: 25,
+                    // borderRadius: 25,
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}
@@ -132,13 +174,26 @@ const Main = () => {
                     setModalVisible(true);
                 }}
             >
-                <Text style={{ color: '#fff', fontSize: 30 }}>+</Text>
+                {/* <Text style={{ color: '#fff', fontSize: 30 }}>+</Text> */}
+                <ImageBackground style={{
+                    height: 50,
+                    width: 50,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                    source={require("../images/folder.png")}>
+                    <Text style={{ fontSize: 40 }}>+</Text>
+                </ImageBackground>
             </TouchableOpacity>
             <TouchableOpacity
                 style={{
                     position: 'absolute',
                     right: 20,
-                    
+                    bottom: 130,
+                    // backgroundColor: '#000',
+                    width: 50,
+                    height: 50,
+                    // borderRadius: 25,
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}
@@ -146,7 +201,10 @@ const Main = () => {
                     setModalVisible2(true);
                 }}
             >
-                <Text style={{ color: '#fff', fontSize: 30 }}>cf</Text>
+                {/* <Text style={{ color: '#fff', fontSize: 30 }}>cf</Text> */}
+                <ImageBackground style={{ height: 50, width: 50, justifyContent: 'center', alignItems: 'center' }} source={require("../images/icon-file.png")}>
+                    <Text style={{ fontSize: 40 }}>+</Text>
+                </ImageBackground>
             </TouchableOpacity>
 
             <Modal
@@ -154,6 +212,81 @@ const Main = () => {
                 visible={modalVisible}
                 onRequestClose={() => {
                     setModalVisible(false);
+                }}
+            >
+
+                <View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0,
+                        backgroundColor: 'rgba(0,0,0,0.5)',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+
+                    <View
+                        style={{
+                            backgroundColor: '#fff',
+                            width: '90%',
+                            height: 200,
+                            borderRadius: 10,
+                        }}
+                    >
+                        <Text style={{
+                            fontSize: 20,
+                            fontWeight: 'bold',
+                            marginLeft: 20,
+                            marginTop: 15
+                        }}>Add New Folder</Text>
+
+                        <TextInput
+                            placeholder="Enter Folder Name"
+                            value={folderName}
+                            onChangeText={(txt) => setFolderName(txt)}
+                            style={{
+                                width: '90%',
+                                height: 50,
+                                borderWidth: 1,
+                                alignSelf: 'center',
+                                marginTop: 20,
+                                paddingLeft: 20,
+                                borderRadius: 10,
+
+                            }}
+                        />
+                        <TouchableOpacity
+                            style={{
+                                marginTop: 25,
+                                alignSelf: 'center',
+                                width: '90%',
+                                height: 50,
+                                borderRadius: 10,
+                                backgroundColor: '#000',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+
+                            }}
+                            onPress={() => {
+                                setModalVisible(false);
+                                createFolder();
+                                setFolderName('');
+                                
+                            }}
+                        >
+                            <Text style={{ color: '#fff', fontSize: 18 }}>Create Folder</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+            <Modal
+                transparent
+                visible={modalVisible2}
+                onRequestClose={() => {
+                    setModalVisible2(false);
                 }}
             >
                 <View
@@ -176,23 +309,29 @@ const Main = () => {
                             borderRadius: 10,
                         }}
                     >
+                        <Text style={{
+                            fontSize: 20,
+                            fontWeight: 'bold',
+                            marginLeft: 20,
+                            marginTop: 15
+                        }}>Add New File</Text>
                         <TextInput
-                            placeholder="Enter Folder Name"
-                            value={folderName}
-                            onChangeText={(txt) => setFolderName(txt)}
+                            placeholder="Enter File Name"
+                            value={fileName}
+                            onChangeText={(txt) => setFileName(txt)}
                             style={{
                                 width: '90%',
                                 height: 50,
                                 borderWidth: 1,
                                 alignSelf: 'center',
-                                marginTop: 50,
+                                marginTop: 20,
                                 paddingLeft: 20,
                                 borderRadius: 10,
                             }}
                         />
                         <TouchableOpacity
                             style={{
-                                marginTop: 20,
+                                marginTop: 25,
                                 alignSelf: 'center',
                                 width: '90%',
                                 height: 50,
@@ -201,22 +340,21 @@ const Main = () => {
                                 justifyContent: 'center',
                                 alignItems: 'center',
                             }}
-                          onPress={() => {
-                                setModalVisible(false);
-                                createFolder();
-                                setFolderName('');
+                            onPress={() => {
+                                setModalVisible2(false);
+                                createFile();
+                                setFileName('')
+
+
                             }}
                         >
-                            <Text style={{ color: '#fff', fontSize: 18 }}>Create Folder</Text>
+                            <Text style={{ color: '#fff', fontSize: 18 }}>Create File</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             </Modal>
-             
         </View>
     );
 };
 
 export default Main;
-
-
